42e5380867b3f12db56a9adacc53d203e7f18292
Anusuya Bhattacharjee <anusuya@appstango.com>
2024-09-09 21:04:35 +0530

    header-done

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='bg-blue-500 w-full py-[100px]'>
+<div className='max-w-[1240px] mx-auto text-center font-bold'>
+{/* <div className='grid justify-items-center'> */}
+<div className='text-xl md:text-3xl md:mb-5'>Learn With Us</div>
+<div className=' text-3xl md:text-5xl text-white md:mb-5'>Grow With Us</div>
+<div className='text-xl md:text-3xl text-white'>Learn Ethical Hack</div>
+{/* // </div> */}
+
+</div>
+    </div>
+  )
+}
+
+export default Banner
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Header</div>
+    <div className='bg-blue-500 p-4'>
+      <div className='max-w-[1240px] flex justify-between items-center py-[12px] mx-auto'>
+        <div className='text-3xl font-bold'>
+          WsCube Tech
+        </div>
+        {
+          toggle ? 
+          <AiOutlineClose onClick={()=>setToggle(!toggle)} className='text-white text-2xl md:hidden block'/>
+          :
+          <AiOutlineMenu onClick={()=>setToggle(!toggle)} className='text-white text-2xl md:hidden block'/>
+        }
+        <ul className='hidden md:flex text-white gap-5'>
+          <li>Home</li>
+          <li>Company</li>
+          <li>Resource</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        {/* Responsive menu */}
+        <ul className={`duration-300 md:hidden w-full h-screen text-white bg-black fixed top-[92px]
+        ${toggle ? 'left-[0]' : 'left-[-100%]'}`}>
+          <li className='p-5'>Home</li>
+          <li className='p-5'>Company</li>
+          <li className='p-5'>Resource</li>
+          <li className='p-5'>About</li>
+          <li className='p-5'>Contact</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
