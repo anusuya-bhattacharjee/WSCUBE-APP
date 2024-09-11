1d4da2edc2e1139a5a53d99b5f11d910d4ec7a14
Anusuya Bhattacharjee <anusuya@appstango.com>
2024-09-11 12:11:10 +0530

    NewsLetter-part-done

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const NewsLetter = () => {
   return (
-    <div>NewsLetter</div>
+    <div className='bg-blue-500 w-full p-4'>
+    <div className='max-w-screen-xl md:flex justify-around mx-auto py-12'>
+    <div className='mt-2'>
+        <h1 className='text-xl md:text-3xl text-white font-bold'>Want to learn latest I.T skills?</h1>
+        <p className='text-white'>Sign up to our newsletter and stay up to date.</p>
+      </div>
+      <div className='mt-2'>
+        <input className='p-2 rounded text-slate-500 mb-2 mr-2' type="text" placeholder='Enter Email'/>
+        <button className='bg-black text-white p-2 rounded font-bold'>Notify Me</button>
+        <p className='text-white'>We care about the protection of your data. Read Our</p>
+        <button>Privacy Policy</button>
+      </div>
+    </div>
+    </div>
   )
 }
 
